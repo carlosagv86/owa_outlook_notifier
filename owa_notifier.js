006a72091f60c8ffcea9a194b21f67af81e82439
carlosagv86 <carlosagv86@gmail.com>
2020-06-09 13:22:09 -0300

    Update owa_notifier.js

--- owa_notifier.js
+++ owa_notifier.js
@@ -1,42 +1,55 @@
+function getElementFromXPath(xpath, doc) {
+    return document.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
 var xpath_messages_list = '//div[contains(@tempid,"emailslistviewpanel")]';
+
 function get_unread_messages() {
-    var clist = document.evaluate(xpath_messages_list, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    var clist = getElementFromXPath(xpath_messages_list, document);
 
     if (clist) {
-        var messages_read = localStorage.getItem('messages_read');
-        if(!messages_read) messages_read = []
-
         for (var i=0; i<clist.childNodes.length; i++) {
             if(clist.childNodes.length>0) {
                 if(clist.childNodes[i].getAttribute('id') != null && clist.childNodes[i].getAttribute('id').startsWith('_')) {
-                    var isnew = document.evaluate('div[1]/div[4]', clist.childNodes[i], null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.classList.contains('ms-font-weight-semibold');
-                    if(isnew) {
-                        var from = document.evaluate('div[1]/div[3]/div[1]/span[2]', clist.childNodes[i], null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
-                        var subject = document.evaluate('div[1]/div[4]/div[2]/span[3]', clist.childNodes[i], null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
-                        var preview = document.evaluate('div[1]/div[5]/div[1]/span[1]', clist.childNodes[i], null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
-                        if(messages_read.includes(subject)) {
-                            console.log('skipping ' + subject)
-                            continue;
+                    var is_new = getElementFromXPath('div[1]/div[4]', clist.childNodes[i]).classList.contains('ms-font-weight-semibold');
+                    if(is_new) {
+                        var from = getElementFromXPath('div[1]/div[3]/div[1]/span[2]', clist.childNodes[i]).innerText;
+                        var subject = getElementFromXPath('div[1]/div[4]/div[2]/span[3]', clist.childNodes[i]).innerText;
+                        var preview = getElementFromXPath('div[1]/div[5]/div[1]/span[1]', clist.childNodes[i]).innerText;
+
+                        console.log(from, subject, preview);
+                        
+                        var messages_read = localStorage.getItem('messages_read');
+                        if(messages_read) {
+                            messages_read = JSON.parse(messages_read.split(','));
+                            if(messages_read.includes(subject)) {
+                                continue;
+                            }
+
+                            if(!messages_read) {
+                                messages_read = []
+                            }
+
+                            var options = {
+                                body: subject + ': ' + preview,
+                                icon: document.querySelector('link[rel="shortcut icon"]').href,
+                                requireInteraction: true
+                            };
+                            var notification = new Notification(from, options);
+                            notification.onclick = function() {
+                                window.focus();
+                            };
+
+                            messages_read.push(subject);
+                            localStorage.setItem('messages_read', JSON.stringify(messages_read));
                         }
-
-                        console.log(isnew, from, subject, preview);
-
-                        var options = {
-                            body: subject + ': ' + preview,
-                            icon: document.querySelector('link[rel="shortcut icon"]').href,
-                            requireInteraction: true
-                        };
-                        var notification = new Notification(from, options);
-                        notification.onclick = function() {
-                            window.focus();
-                        };
-
-                        messages_read.push(subject);
-                        localStorage.setItem('messages_read', messages_read)
                     }
                 }
             }
         }
     }
 }
+
+localStorage.setItem('messages_read', JSON.stringify([]));
+
 var check_mails = setInterval(get_unread_messages, 2500);
